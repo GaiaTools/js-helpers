97dba901ae38c8bdf074c6f73109146ccff596be
Correct Name <6134576+knightyoshi@users.noreply.github.com>
2020-12-21 01:13:45 +0000

    refactor: use CJS exports

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
-import DomParser from './DomParser';
-import Html from './Html';
-import ObjectHelper from './ObjectHelper';
+//import DomParser from './DomParser';
+//import Html from './Html';
+//import ObjectHelper from './ObjectHelper';
 
-export {DomParser, Html, ObjectHelper};
+//export {DomParser, Html, ObjectHelper};
+
+const DomParser = require('./DomParser');
+const Html = require('./Html');
+const ObjectHelper = require('./ObjectHelper');
+
+module.exports = {DomParser, Html, ObjectHelper};
