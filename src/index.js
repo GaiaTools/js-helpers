3efb4f2c77b63397ff8497dfe22c1317498df25b
Knight Yoshi <6134576+KnightYoshi@users.noreply.github.com>
2024-03-15 07:03:24 -0400

    feat: update named exports

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 export {DomParser} from './DomParser';
-export {Html} from './Html';
 export {ObjectHelper} from './ObjectHelper';
+
+// Auto-generated exports for Html
+export {
+  tag,
+  a,
+  img,
+  form,
+  label,
+  button,
+  resetButton,
+  submitButton,
+  input,
+  buttonInput,
+  submitInput,
+  resetInput,
+  textInput,
+  hiddenInput,
+  passwordInput,
+  fileInput,
+  textarea,
+  radio,
+  checkbox,
+  booleanInput,
+  renderContent,
+  select,
+  listBox,
+  renderSelectOptions,
+  checkboxList,
+  radioList,
+  ul,
+  ol,
+  text,
+  setAttributes,
+  addClass,
+  removeClass
+} from './Html';
